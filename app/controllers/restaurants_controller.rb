107e01aa2d117292f9a3ea53df7fefe8a505e65a
John Davison <johncdavison@gmail.com>
2014-02-28 07:17:40 -0800

    Ui framework in place.

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,22 @@
+class RestaurantsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
